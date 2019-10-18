c91cb78e9900c89ce189e51b7b6da26763b03ddb
JohnFury77 <furijani99@gmail.com>
2019-10-18 12:54:36 +0200

    1

--- RegisztracioAlkalmazas/Form1.Designer.cs
+++ RegisztracioAlkalmazas/Form1.Designer.cs
@@ -28,13 +28,203 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.wwwwwwwwwww = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.Hozzaadas = new System.Windows.Forms.Button();
+            this.Mentes = new System.Windows.Forms.Button();
+            this.Betöltés = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.SuspendLayout();
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(106, 71);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(37, 21);
+            this.radioButton1.TabIndex = 0;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "F";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(167, 71);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(39, 21);
+            this.radioButton2.TabIndex = 1;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "N";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(86, 112);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(120, 84);
+            this.listBox1.TabIndex = 2;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // wwwwwwwwwww
+            // 
+            this.wwwwwwwwwww.AutoSize = true;
+            this.wwwwwwwwwww.Location = new System.Drawing.Point(12, 15);
+            this.wwwwwwwwwww.Name = "wwwwwwwwwww";
+            this.wwwwwwwwwww.Size = new System.Drawing.Size(37, 17);
+            this.wwwwwwwwwww.TabIndex = 3;
+            this.wwwwwwwwwww.Text = "Név:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 43);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(88, 17);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Szül. Dátum:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 112);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(49, 17);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Hobbi:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 66);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(41, 17);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Nem:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 207);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(66, 17);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "Új Hobbi:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(86, 202);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(120, 22);
+            this.textBox1.TabIndex = 8;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(106, 15);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 22);
+            this.textBox2.TabIndex = 9;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(106, 43);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 22);
+            this.textBox3.TabIndex = 10;
+            // 
+            // Hozzaadas
+            // 
+            this.Hozzaadas.Location = new System.Drawing.Point(86, 230);
+            this.Hozzaadas.Name = "Hozzaadas";
+            this.Hozzaadas.Size = new System.Drawing.Size(120, 23);
+            this.Hozzaadas.TabIndex = 11;
+            this.Hozzaadas.Text = "Hozzáadás";
+            this.Hozzaadas.UseVisualStyleBackColor = true;
+            this.Hozzaadas.Click += new System.EventHandler(this.Hozzaadas_Click);
+            // 
+            // Mentes
+            // 
+            this.Mentes.Location = new System.Drawing.Point(50, 286);
+            this.Mentes.Name = "Mentes";
+            this.Mentes.Size = new System.Drawing.Size(75, 23);
+            this.Mentes.TabIndex = 12;
+            this.Mentes.Text = "Mentés";
+            this.Mentes.UseVisualStyleBackColor = true;
+            // 
+            // Betöltés
+            // 
+            this.Betöltés.Location = new System.Drawing.Point(131, 286);
+            this.Betöltés.Name = "Betöltés";
+            this.Betöltés.Size = new System.Drawing.Size(75, 23);
+            this.Betöltés.TabIndex = 13;
+            this.Betöltés.Text = "Betoltes";
+            this.Betöltés.UseVisualStyleBackColor = true;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // saveFileDialog1
+            // 
+            this.saveFileDialog1.FileName = "+";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(221, 352);
+            this.Controls.Add(this.Betöltés);
+            this.Controls.Add(this.Mentes);
+            this.Controls.Add(this.Hozzaadas);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.wwwwwwwwwww);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label wwwwwwwwwww;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button Hozzaadas;
+        private System.Windows.Forms.Button Mentes;
+        private System.Windows.Forms.Button Betöltés;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
     }
 }
 
--- RegisztracioAlkalmazas/Form1.cs
+++ RegisztracioAlkalmazas/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,6 +16,64 @@ namespace RegisztracioAlkalmazas
         public Form1()
         {
             InitializeComponent();
+            listBox1.Items.Add("Úszás");
+            listBox1.Items.Add("Horgászat");
+            listBox1.Items.Add("Futás");
+
+            saveFileDialog1.FileOk += (senderFile, eFile) => 
+            {
+                try
+                {
+                    string fileName = saveFileDialog1.FileName;
+                    File.WriteAllLines(fileName, listBox1.Items.Cast<String>().ToArray()); 
+                 
+                }
+                catch (Exception)
+                {
+
+                    MessageBox.Show("Hiba! Nem sikerült a kiírás");
+                }
+            };
+
+           
+            openFileDialog1.FileOk += (sender, e) =>
+            {
+                try
+                {
+                    string[] sorok = File.ReadAllLines(openFileDialog1.FileName);
+                    
+                    listBox1.Items.Clear();
+                    foreach (var item in sorok) 
+                    {
+                        listBox1.Items.Add(item);
+                    }
+                }
+                catch (IOException)
+                {
+                    MessageBox.Show("Hiba! sikertelen betöltés.");
+                }
+            };
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Hozzaadas_Click(object sender, EventArgs e)
+        {           
+                listBox1.Items.Add(textBox1.Text);
+                textBox1.Text = "";           
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
         }
     }
 }
